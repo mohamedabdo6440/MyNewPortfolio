a96f75cb5f3b54d7c1fbd9c334281633a09e7085
Mohamed Abdo <102926241+mohamedabdo6440@users.noreply.github.com>
2023-03-11 13:46:00 +0200

    handle pwa files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,8 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <img
-        onError={(e) => {
-          e.target.remove();
-        }}
-        onLoad={(e) => {
-          e.target.value = logo;
-        }}
-        src={logo}
-        className="App-logo"
-        alt="logo"
-      />
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
