7d93e8fb46a7bb8f9a86e3e188e87e7d1aea6a3c
Mohamed Abdo <102926241+mohamedabdo6440@users.noreply.github.com>
2023-03-29 06:51:16 +0200

    add database file

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,21 +1,21 @@
 import { initializeApp } from "firebase/app";
-import {  getAuth,signInWithPopup, signOut } from "firebase/auth"
-import { getFirestore } from "firebase/firestore"
+import { getAuth, signInWithPopup, signOut } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
-  apiKey: "AIzaSyDxsfSM91vfVpoM1UMbs_4ABMmy3nzWpG8",
-  authDomain: "protofolio-64812.firebaseapp.com",
-  projectId: "protofolio-64812",
-  storageBucket: "protofolio-64812.appspot.com",
-  messagingSenderId: "678049566053",
-  appId: "1:678049566053:web:a0f6fcd2111706e451658c"
+  apiKey: "AIzaSyCJdiJGpJnnr-hfitLYzw2uGeYC5VVUkfI",
+  authDomain: "myportfolio-2f63b.firebaseapp.com",
+  projectId: "myportfolio-2f63b",
+  storageBucket: "myportfolio-2f63b.appspot.com",
+  messagingSenderId: "1090375032512",
+  appId: "1:1090375032512:web:1d5a162d09c26cf4715725",
 };
 
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app)
-export const db = getFirestore(app)
-export function signInWithGoogle( password) {
-    return signInWithPopup(auth, password)
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export function signInWithGoogle(password) {
+  return signInWithPopup(auth, password);
 }
 export function singout() {
-  return signOut(auth)
+  return signOut(auth);
 }
